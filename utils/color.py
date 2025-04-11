53988b549d405d97548d66bfc94f331cdba9066c
Omar <omarislamkhfaga@gmail.com>
2025-04-11 15:04:17 +0200

    Added External Color file

---
+++ utils/color.py
@@ -0,0 +1,14 @@
+class Colors:
+    RESET = "\033[0m"
+    # Red For Error
+    RED = "\033[91m"
+    # Green For Success
+    GREEN = "\033[92m"
+    # Yellow For Warning
+    YELLOW = "\033[93m"
+    # Blue For Info
+    BLUE = "\033[94m"
+    # For Banner
+    CYAN = "\033[96m"
+    # Magenta for Processing dorking
+    MAGENTA = "\033[95m"
